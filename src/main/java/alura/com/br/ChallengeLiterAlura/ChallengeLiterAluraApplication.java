fbe80342673c90cd6499f3c5ba8aacd607a5cd99
Eliseu Brito <eliseubrito@protonmail.com>
2025-08-18 19:13:14 -0300

    Adicionado configuracao base e menu base

---
+++ src/main/java/alura/com/br/ChallengeLiterAlura/ChallengeLiterAluraApplication.java
@@ -0,0 +1,22 @@
+package alura.com.br.ChallengeLiterAlura;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import alura.com.br.ChallengeLiterAlura.principal.Principal;
+
+@SpringBootApplication
+public class ChallengeLiterAluraApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
+
+}
---
+++ src/test/java/alura/com/br/ChallengeLiterAlura/ChallengeLiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package alura.com.br.ChallengeLiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
