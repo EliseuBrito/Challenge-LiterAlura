40b2ff44f7aa66ba3ff712a47bb6f15dd189f076
Eliseu Brito <eliseubrito@protonmail.com>
2025-08-19 22:48:57 -0300

    corrigido repositorio autor e livro

--- src/main/java/alura/com/br/ChallengeLiterAlura/repository/AutorRepository.java
+++ src/main/java/alura/com/br/ChallengeLiterAlura/repository/AutorRepository.java
@@ -1,6 +1,34 @@
 package alura.com.br.ChallengeLiterAlura.repository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import alura.com.br.ChallengeLiterAlura.model.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    @Query("""
+           SELECT DISTINCT autor FROM Autor autor
+           LEFT JOIN FETCH autor.livros
+           """)
+    List<Autor> listarTodosComLivros();
+
+    @Query("""
+           SELECT DISTINCT autor FROM Autor autor
+           LEFT JOIN FETCH autor.livros
+           WHERE (autor.dataNascimento IS NULL OR autor.dataNascimento <= :ano)
+             AND (autor.dataMorte IS NULL OR autor.dataMorte >= :ano)
+           """)
+    List<Autor> obterAutoresVivosComLivrosNoAno(Integer ano);
+
+    @Query("""
+           SELECT autor FROM Autor autor
+           WHERE (autor.dataNascimento IS NULL OR autor.dataNascimento <= :ano)
+             AND (autor.dataMorte IS NULL OR autor.dataMorte >= :ano)
+           """)
+    List<Autor> filtrarVivosPorAno(Integer ano);
+
+    Optional<Autor> findByNomeIgnoreCase(String nome);
 }
--- src/main/java/alura/com/br/ChallengeLiterAlura/repository/LivroRepository.java
+++ src/main/java/alura/com/br/ChallengeLiterAlura/repository/LivroRepository.java
@@ -1,8 +1,27 @@
 package alura.com.br.ChallengeLiterAlura.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import alura.com.br.ChallengeLiterAlura.model.Livro;
 
-
+import java.util.List;
+import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    @Query("""
+           SELECT livro FROM Livro livro
+           LEFT JOIN FETCH livro.autor
+           """)
+    List<Livro> buscarTodosComAutor();
+
+    @Query("""
+           SELECT livro FROM Livro livro
+           LEFT JOIN FETCH livro.autor
+           WHERE UPPER(livro.idioma) = UPPER(:idioma)
+           """)
+    List<Livro> buscarPorIdiomaComAutor(String idioma);
+
+    Optional<Livro> findByTituloIgnoreCase(String titulo);
 }
